dee60b19ad9e3ee0595d4867d667dcdc90f13735
Recep Taylanhan <rcptay@gmail.com>
2026-01-15 14:40:42 -0500

    added mirac

---
+++ mocjjson.js
@@ -0,0 +1,92 @@
+[
+  {
+    "id": 2,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image2.jpg",
+    "theme_tr": "TEVEKKÜL",
+    "theme_en": "TRUST",
+    "title_tr": "Hasbunallahu ve Ni'mel Vekil",
+    "title_en": "Allah is Sufficient for Us",
+    "text_ar": "حَسْبُنَا اللَّهُ وَنِعْمَ الْوَكِيلُ نِعْمَ الْمَوْلَى وَنِعْمَ النَّصِيرُ",
+    "translation_tr": "Allah bize yeter, O ne güzel vekildir. O ne güzel dost ve ne güzel yardımcıdır.",
+    "translation_en": "Allah is sufficient for us, and He is the best Disposer of affairs. What an excellent Protector and Helper."
+  },
+  {
+    "id": 3,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image4.jpg",
+    "theme_tr": "KORUNMA",
+    "theme_en": "PROTECTION",
+    "title_tr": "Bismillahillezi...",
+    "title_en": "In the Name of Allah...",
+    "text_ar": "بِسْمِ اللَّهِ الَّذِي لَا يَضُرُّ مَعَ اسْمِهِ شَيْءٌ فِي الْأَرْضِ وَلَا فِي السَّمَاءِ وَهُوَ السَّمِيعُ الْعَلِيمُ",
+    "translation_tr": "İsmiyle yerde ve gökte hiçbir şeyin zarar veremeyeceği Allah'ın adıyla. O, hakkıyla işitendir, hakkıyla bilendir.",
+    "translation_en": "In the name of Allah, with Whose name nothing on earth or in the heaven can cause any harm. He is the All-Hearing, All-Knowing."
+  },
+  {
+    "id": 4,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image5.jpg",
+    "theme_tr": "DÜNYA VE AHİRET",
+    "theme_en": "WORLD & HEREAFTER",
+    "title_tr": "Rabbena Atina",
+    "title_en": "Our Lord, Grant Us",
+    "text_ar": "رَبَّنَا آتِنَا فِي الدُّنْيَا حَسَنَةً وَفِي الْآخِرَةِ حَسَنَةً وَقِنَا عَذَابَ النَّارِ",
+    "translation_tr": "Rabbimiz! Bize dünyada da iyilik ver, ahirette de iyilik ver. Bizi cehennem azabından koru.",
+    "translation_en": "Our Lord, give us in this world that which is good and in the Hereafter that which is good and protect us from the punishment of the Fire."
+  },
+  {
+    "id": 5,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image1.jpg",
+    "theme_tr": "BAĞIŞLANMA",
+    "theme_en": "FORGIVENESS",
+    "title_tr": "Rabbenağfirli",
+    "title_en": "Our Lord, Forgive Me",
+    "text_ar": "رَبَّنَا اغْفِرْ لِي وَلِوَالِدَيَّ وَلِلْمُؤْمِنِينَ يَوْمَ يَقُومُ الْحِسَابُ",
+    "translation_tr": "Rabbimiz! Hesap görülecek günde beni, ana-babamı ve müminleri bağışla.",
+    "translation_en": "Our Lord, forgive me and my parents and the believers on the Day the account is established."
+  },
+  {
+    "id": 6,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image2.jpg",
+    "theme_tr": "NAMAZ",
+    "theme_en": "PRAYER",
+    "title_tr": "Ettehiyyatü",
+    "title_en": "At-Tahiyyat",
+    "text_ar": "التَّحِيَّاتُ لِلَّهِ وَالصَّلَواتُ وَالطَّيِّباتُ السَّلامُ عَلَيْكَ أَيُّهَا النَّبِيُّ وَرَحْمَةُ اللَّهِ وَبَرَكاتُهُ السَّلامُ عَلَيْنا وَعَلَى عِبادِ اللَّهِ الصَّالِحينَ",
+    "translation_tr": "Dil, beden ve mal ile yapılan bütün ibadetler Allah'adır. Ey Peygamber! Allah'ın selamı, rahmet ve bereketi senin üzerine olsun.",
+    "translation_en": "All compliments, prayers and pure words are due to Allah. Peace be upon you, O Prophet, and the mercy of Allah and His blessings."
+  },
+  {
+    "id": 7,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image4.jpg",
+    "theme_tr": "GÜNLÜK ADAP",
+    "theme_en": "DAILY ADAB",
+    "title_tr": "Tuvalete Girerken",
+    "title_en": "Entering the Toilet",
+    "text_ar": "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْخُبُثِ وَالْخَبَائِثِ",
+    "translation_tr": "Allah'ım! Pislikten ve cinlerin şerrinden sana sığınırım.",
+    "translation_en": "O Allah, I seek refuge with You from all offensive and wicked things (evil spirits)."
+  },
+  {
+    "id": 8,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image3.jpg",
+    "theme_tr": "GÜNLÜK ADAP",
+    "theme_en": "DAILY ADAB",
+    "title_tr": "Tuvaletten Çıkarken",
+    "title_en": "Leaving the Toilet",
+    "text_ar": "غُفْرَانَكَ",
+    "translation_tr": "Allah'ım senin bağışlamanı dilerim.",
+    "translation_en": "I ask You (Allah) for forgiveness."
+  },
+  {
+    "id": 9,
+    "image_url": "https://raw.githubusercontent.com/Recep-T/ios-json-api/main/src/assets/image1.jpg",
+    "theme_tr": "KOLAYLIK",
+    "theme_en": "EASE",
+    "title_tr": "Rabbi Yessir",
+    "title_en": "Rabbi Yessir",
+    "text_ar": "رَبِّ يَسِّرْ وَلَا تُعَسِّرْ رَبِّ تَمِّمْ بِالْخَيْرِ",
+    "translation_tr": "Rabbim! Kolaylaştır, zorlaştırma. Rabbim, hayırla tamamla.",
+    "translation_en": "My Lord, make it easy and do not make it difficult. My Lord, let it be completed with goodness."
+  }
+]
+
+
